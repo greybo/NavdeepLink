d311ec0b2be54abc2784da27a4806c701a890806
greybo <sergeybotl@gmail.com>
2020-04-01 13:54:50 +0300

    add epoxy

---
+++ app/src/main/java/com/example/m/myapplication/two/SingleFoodController.kt
@@ -0,0 +1,9 @@
+package com.example.m.myapplication.two
+
+import com.airbnb.epoxy.EpoxyController
+
+class SingleFoodController : EpoxyController() {
+    override fun buildModels() {
+        TODO("Not yet implemented")
+    }
+}
